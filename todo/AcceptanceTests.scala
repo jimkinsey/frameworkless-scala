f045f3e1ed884be2e19af66f934c8c03857e4a10
jimkinsey <jim.kinsey@springer.com>
2018-09-23 00:20:38 +0200

    Remove REST endpoints
    The point is to build the HTML GUI, the REST endpoints were an unnecessary distraction.

--- todo/AcceptanceTests.scala
+++ todo/AcceptanceTests.scala
@@ -36,40 +36,6 @@ class AcceptanceTests
     assert("""<li.*>Get milk</li>""".r.findFirstMatchIn(res.body).isDefined)
   }
 
-  def testNoSuchTodo(): Unit = {
-    val res = HTTP.get("http://localhost:9090/todos/doesnt-exist")
-    assert(res.status == 404)
-  }
-
-  def testGetTodo(): Unit = {
-    HTTP.put("http://localhost:9090/todos/exists")
-    val res = HTTP.get("http://localhost:9090/todos/exists")
-    assert(res.status == 200)
-  }
-
-  def testCreatingTodo(): Unit = {
-    val res = HTTP.put("http://localhost:9090/todos/new")
-    assert(res.status == 201)
-  }
-
-  def testDeletingTodo(): Unit = {
-    HTTP.put("http://localhost:9090/todos/exists")
-    val res = HTTP.delete("http://localhost:9090/todos/exists")
-    assert(res.status == 204)
-  }
-
-  def testListTodos() = {
-    HTTP.put("http://localhost:9090/todos/get-milk")
-    HTTP.put("http://localhost:9090/todos/hang-laundry")
-    HTTP.put("http://localhost:9090/todos/put-out-cat")
-    val res = HTTP.get("http://localhost:9090/todos")
-
-    assert(res.body ==
-      """get-milk
-        |hang-laundry
-        |put-out-cat""".stripMargin)
-  }
-
   def tearDown() = {
     server.stop(0)
   }
--- todo/Controller.scala
+++ todo/Controller.scala
@@ -15,23 +15,7 @@ class Controller
         val form = Form.values(req.body)
         store.put(form("name").head)
         Response(201, View.page(store.getAll), Map("Content-Type" -> Seq("text/html; charset=UTF-8")))
-      case ("GET", Todos(id)) =>
-        store.get(id) match {
-          case Some(t) => Response(200)
-          case None => Response(404)
-        }
-      case ("PUT", Todos(id)) =>
-        store.put(id)
-        Response(201)
-      case ("DELETE", Todos(id)) =>
-        store.delete(id)
-        Response(204)
-      case ("GET", TodoList) =>
-        Response(200, store.getAll.mkString("\n"))
       case _ =>
         Response(404)
     }
-
-  private val Todos = """\/todos\/([\w\d-]+)""".r
-  private val TodoList = "/todos"
 }
