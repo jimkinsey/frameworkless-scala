918c9e9a0895dd55539257c4d518b1c15ce99d4a
Jim Kinsey <jim.kinsey@springer.com>
2018-10-05 23:12:44 +0200

    Use scalatags to render HTML

--- src/main/scala/todo/View.scala
+++ src/main/scala/todo/View.scala
@@ -1,39 +1,45 @@
 package todo
 
+import scalatags.Text
 import todo.Model.Item
+import scalatags.Text.all._
 
 object View
 {
+  val section: Text.TypedTag[String] = tag("section")
+  val noscript: Text.TypedTag[String] = tag("noscript")
+
   def page(items: Seq[Item] = Seq.empty, feedback: String = ""): String = {
-    s"""<!DOCTYPE HTML>
-        <html>
-          <head>
-            <title>My Todo List</title>
-            <link rel="stylesheet" type="text/css" href="/static/todo-mvp.css"/>
-          </head>
-          <body>
-           <section aria-labelledby="todos-label">
-             <h1 id="todos-label">My Todo List</h1>
-             <form action="/" method="POST">
-               <ul>${items.zipWithIndex.map { case (item, i) =>
-                 s"""<li>
-                   <input type="checkbox" id="todo-$i"${if (item.done) " checked" else ""} name="${item.id}" onchange="this.form.submit();">
-                   <label for="todo-$i">${item.name}</label>
-                   <button aria-label="delete ${item.name}" name="delete" value="${item.id}">&times;</button>
-                 </li>"""
-               }.mkString}</ul>
-               <div class="empty-state">
-                 <p>Either you've done everything already or there are still things to add to your list. Add your first todo &#x2193;</p>
-               </div>
-               <noscript><button type="submit">Update</button></noscript>
-             </form>
-             <form action="/" method="POST">
-               <input type="text" aria-label="Write a new todo item" placeholder="E.g. Adopt an owl" name="name">
-               <button type="submit">Add</button>
-             </form>
-             <div role="status" aria-live="polite" class="vh">$feedback</div>
-           </section>
-          </body>
-        </html>"""
+
+    "<!DOCTYPE HTML>\n" + html(
+      head(
+        tag("title")("My Todo List"),
+        link(rel := "stylesheet", `type` := "text/css", href := "/static/todo-mvp.css")
+      ),
+      body(
+        section(
+          aria.labelledby := "todos-label",
+          h1(id := "todos-label", "My Todo List"),
+          form(action := "/", method := "POST")(
+            ul(items.zipWithIndex.map { case (item, i) =>
+              li(
+                input(`type` := "checkbox", id := s"todo-$i", (if (item.done) Some(checked) else None).asInstanceOf[Option[Modifier]], name := item.id, onchange := "this.form.submit();"),
+                label(`for` := s"todo-$i")(item.name),
+                button(aria.label := s"delete ${item.name}", name := "delete", value := item.id)(raw("&times;"))
+              )
+            }),
+            div(`class` := "empty-state")(
+              p(raw("Either you've done everything already or there are still things to add to your list. Add your first todo &#x2193;"))
+            ),
+            noscript(button(`type` := "submit")("Update"))
+          ),
+          form(action := "/", method := "POST")(
+            input(`type` := "text", aria.label := "Write a new todo item", placeholder := "E.g. Adopt an owl", name := "name"),
+            button(`type` := "submit")("Add")
+          ),
+          div(role := "status", aria.live := "polite", `class` := "vh")(feedback)
+        )
+      )
+    )
   }
 }
